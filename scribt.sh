5279b7cac6682a0550d2f03783affb539db1ebd8
Kowsikan Sathiyamoorthy <sukkran@hotmail.de>
2017-03-27 10:43:57 +0200

    Init add seq

--- scribt.sh
+++ scribt.sh
@@ -8,4 +8,3 @@ cd Customer
 mvn clean install
 sudo docker run -v $(pwd)/src/test/resources/money.uml:/opt/mbt/xmi.uml -v $(pwd)/src/test/java:/opt/mbt/classes mbt-core
 sudo docker build -t customer .
-sudo docker run -v $(pwd)/src/test/resources/deploy.uml:/opt/mbt/xmi.uml mbt-deploy
