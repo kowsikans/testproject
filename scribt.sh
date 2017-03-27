808a5a3b16189b2a8d59a5be4cb3f5bac1a77242
Kowsikan Sathiyamoorthy <sukkran@hotmail.de>
2017-03-27 10:33:06 +0200

    Init add seq

--- scribt.sh
+++ scribt.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
+
+cd MySQL
+sudo docker build -t custom-mysql .
+cd ..
+
 cd Customer
 mvn clean install
 sudo docker run -v $(pwd)/src/test/resources/money.uml:/opt/mbt/xmi.uml -v $(pwd)/src/test/java:/opt/mbt/classes mbt-core
 sudo docker build -t customer .
-cd ..
-cd MySQL
-sudo docker build -t custom-mysql .
 sudo docker run -v $(pwd)/src/test/resources/deploy.uml:/opt/mbt/xmi.uml mbt-deploy
